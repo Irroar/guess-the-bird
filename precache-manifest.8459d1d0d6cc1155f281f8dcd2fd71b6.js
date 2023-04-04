514213f5d330427c1f3b3fd3021b6edf20953a16
Iryna Lytvynenko <frau4145@gmail.com>
2023-04-04 17:19:51 -0400

    Updates

---
+++ precache-manifest.8459d1d0d6cc1155f281f8dcd2fd71b6.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05fcfbaff6707caa30cc7ca49646b1d1",
+    "url": "/guess-the-bird/index.html"
+  },
+  {
+    "revision": "aaadb755f6ab24918f4b",
+    "url": "/guess-the-bird/static/css/2.00e6e7f4.chunk.css"
+  },
+  {
+    "revision": "a2500502d0f0b6887601",
+    "url": "/guess-the-bird/static/css/main.596f1e34.chunk.css"
+  },
+  {
+    "revision": "aaadb755f6ab24918f4b",
+    "url": "/guess-the-bird/static/js/2.75c0ee19.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/guess-the-bird/static/js/2.75c0ee19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2500502d0f0b6887601",
+    "url": "/guess-the-bird/static/js/main.bb758e2d.chunk.js"
+  },
+  {
+    "revision": "a6decc853478b9f0573b",
+    "url": "/guess-the-bird/static/js/runtime-main.5e8994bb.js"
+  },
+  {
+    "revision": "ed784538d599bf3b5c397212350a5f91",
+    "url": "/guess-the-bird/static/media/congratulationsWithAbsoluteWin.ed784538.jpg"
+  },
+  {
+    "revision": "06a469380cb09a64d372f773665e7500",
+    "url": "/guess-the-bird/static/media/defaultBirdImage.06a46938.jpg"
+  },
+  {
+    "revision": "18423bbf3f91d01afc65e4c858760423",
+    "url": "/guess-the-bird/static/media/failure.18423bbf.mp3"
+  },
+  {
+    "revision": "4f82cd73f3446ef1af7fa6d3a302ef3a",
+    "url": "/guess-the-bird/static/media/logo.4f82cd73.svg"
+  },
+  {
+    "revision": "2b6ac0c25cfb806a19435ae712d544bd",
+    "url": "/guess-the-bird/static/media/pause.2b6ac0c2.svg"
+  },
+  {
+    "revision": "3063680c1653ff79ff748011388ea055",
+    "url": "/guess-the-bird/static/media/play.3063680c.svg"
+  },
+  {
+    "revision": "beda7e18ed738e1ce4d01e0d38d795af",
+    "url": "/guess-the-bird/static/media/success.beda7e18.mp3"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.dd04a3bc502e0b60670889cda18d07e2.js"
+  "/guess-the-bird/precache-manifest.8459d1d0d6cc1155f281f8dcd2fd71b6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/guess-the-bird/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
